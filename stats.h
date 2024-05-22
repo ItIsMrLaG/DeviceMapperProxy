b1d5bab167c249c77bfe1b70c44bd0341d7d5269
ItIsMrLaG <it.is.mr.lag@gmail.com>
2024-05-22 18:46:23 +0000

    feat: Dmp without beautiful statistics

---
+++ stats.h
@@ -0,0 +1,32 @@
+#ifndef STATS_H
+#define STATS_H
+
+#include <linux/kobject.h>
+#include <linux/spinlock_types.h>
+
+struct dmp_stats {
+	long long r_cnt;
+	long long w_cnt;
+
+	long long r_blsize_cnt;
+	long long w_blsize_cnt;
+
+	spinlock_t rlock;
+	spinlock_t wlock;
+
+	struct kobject kobj;
+};
+#define to_dmp_stats(ptr) container_of(ptr, struct dmp_stats, kobj)
+
+struct dmp_stats_attribute {
+	struct attribute attr;
+	ssize_t (*show)(struct dmp_stats *stats, char *buf);
+};
+#define to_dmp_stats_attribute(ptr) \
+	container_of(ptr, struct dmp_stats_attribute, attr)
+
+struct dmp_stats *dmp_create_stats(const char *name, struct kset *kset);
+
+void dmp_destroy_stats(struct dmp_stats *stats);
+
+#endif /* STATS_H */
